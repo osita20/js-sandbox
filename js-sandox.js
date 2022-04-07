ad05447f4436429750124aa58078c4d0667f217b
osita20 <100966996+osita20@users.noreply.github.com>
2022-04-07 20:58:04 +0100

    Add files via upload

---
+++ js-sandox.js
@@ -0,0 +1,15 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.shift(); // 1. Remove the Banana from the array.
+array
+
+
+array                 //to see the items
+array.sort();        // to Sort the array in order
+array.push("Kiwi");  //  to Put "Kiwi" at the end of the array
+array.shift();       // to Remove "Apples" from the array
+
+array.reverse();     // to  Sort the array in reverse order
+
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+array2[1][1] 
