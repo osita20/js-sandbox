28a414f658bab7932e2260ace97f7be92ca1c485
osita20 <100966996+osita20@users.noreply.github.com>
2022-04-08 19:27:12 +0100

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,30 @@
+//create an object that has properties 'username' and 'password'. Fill those values with strings.
+
+var employee = {
+    username:'james',
+    password:'bee123'
+};
+//create an array which contains the objects you made above and name the array 'database'.
+
+var database = [{
+    username:'james',
+    password:'bee123'
+}];
+
+//create an array called 'newsfeed' which can contain 3 objects with properties 'username' and 'timeline'
+
+var newsfeed = [
+    {
+        username:'timothy',
+        timeline: '2007'
+    },
+    
+    {
+        username:'mark',
+        timeline: '2016'
+    },
+    {
+        username:'queen',
+        timeline: '2019'
+    }
+];
