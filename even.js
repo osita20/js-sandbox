fefc18ed50bb2f8c252af94cc741d128ea3c8d97
osita20 <100966996+osita20@users.noreply.github.com>
2022-04-12 18:28:05 +0100

    Create even.js

---
+++ even.js
@@ -0,0 +1,12 @@
+function evenNumber(){
+    var sum =0;
+    for(i=1; i<=100;i++){
+        if(i%2===0){
+            sum = sum+i;
+        };
+    };
+       return sum;
+};
+//document.write(evenNumber())
+//console.log(evenNumber())
+alert("The sum of evenNumber is " + evenNumber())
